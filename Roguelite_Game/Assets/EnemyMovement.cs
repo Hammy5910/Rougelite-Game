f6786fa928ef58efb2822b17f6e7d0ad7ed3fbd4
TheGAPCode <bajwamohid@gmail.com>
2022-07-22 22:49:23 -0500

    Enemy Movement rehash pt1

--- Roguelite_Game/Assets/EnemyMovement.cs
+++ Roguelite_Game/Assets/EnemyMovement.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI; //NavMesh
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    //Patroling
+    public Vector3 walkpoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    //states
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
     {
-        
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Patroling()
     {
-        
+        if (!walkPointSet) SearchWalkPoint();
+
     }
+    private void SearchwalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+    }
+
+    private void ChasePlayer()
+    {
+
+    }
+    private void AttackPlayer()
+    {
+
+    }
+
 }
