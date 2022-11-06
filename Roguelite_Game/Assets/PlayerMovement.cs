75aa493c0bbfa314a3c4a5d1ff702da028c9320f
Hammy5910 <hamza.j.beg@gmail.com>
2022-11-06 13:55:22 -0600

    Raycasting
    Raycasting is mildly functional
    
    ground and air physics need fixing

--- Roguelite_Game/Assets/PlayerMovement.cs
+++ Roguelite_Game/Assets/PlayerMovement.cs
@@ -18,10 +18,20 @@ public class PlayerMovement : MonoBehaviour
 
     public float groundDrag;
 
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
     //GroundCheck
     public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
+
+
+    //Keybinds
+    public KeyCode jumpKey = KeyCode.Space;
+
 
 
     // Start is called before the first frame update
@@ -37,39 +47,52 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //Ground Check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.8f + 0.2f, whatIsGround);
+
+        Debug.DrawRay(transform.position, Vector3.down, Color.blue, whatIsGround);
+
+        if (grounded)
+            print("ground!");
+        else
+            print("not ground!");
 
         MyInput();
         SpeedControl();
 
-        // Handle Drag
+        //Handle Drag
         if (grounded)
             rb.drag = groundDrag;
         else
             rb.drag = 0;
 
+        //Calculate Movement Direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
+        //On Ground
+        if(grounded)
         rb.AddForce(moveDirection.normalized * moveSpeed * 100f * Time.deltaTime, ForceMode.Force);
 
-    }
+        //In Air
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 100f * airMultiplier * Time.deltaTime, ForceMode.Force);
 
-   /* private void FixedUpdate()
-    {
-        MovePlayer();
-    }*/
+    }
 
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-    }
-    /*private void MovePlayer()
-    {
 
-        // calculate movement direction
-        
-    }*/
+        //When to Jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
 
     private void SpeedControl()
     {
@@ -82,4 +105,21 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
+
+    private void Jump()
+    {
+
+        //Reset Y Velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
 }
--- Roguelite_Game/Assets/Scripts/Player/MouseLook.cs
+++ Roguelite_Game/Assets/Scripts/Player/MouseLook.cs
@@ -5,30 +5,87 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 100f;
+    public float sensX;
+    public float sensY;
 
-    public Transform playerBody;
+    public Transform orientation;
 
-    float xRotation = 0f;
+    float xRotation;
+    float yRotation;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        //Locks Cursor to Center of Screen
+
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void Update()
     {
-        //Look Around
-        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
-    } 
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*    public float mouseSensitivity = 100f;
+
+        public Transform playerBody;
+
+        float xRotation = 0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Locks Cursor to Center of Screen
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            //Look Around
+            float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        } */
 }
